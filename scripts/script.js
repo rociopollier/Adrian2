8d9ef6bd8b44005efe7ac5d0c7faf08fd85e77c5
rociopollier <rociopollier97@gmail.com>
2025-03-24 10:46:59 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@ document.querySelector("#formulario-contacto").addEventListener("submit", functi
     let email = document.querySelector("#email").value;
     let mensaje = document.querySelector("#mensaje").value;
     
-    let whatsappURL = `https://api.whatsapp.com/send?phone=5493547514197&text=Hola,%20soy%20${nombre}.%20Mi%20correo%20es%20${email}.%20Mensaje:%20${mensaje}`;
+    let whatsappURL = `https://api.whatsapp.com/send?phone=5493547514197&text=Hola,%20soy%20${nombre}%20${apellido}.%20Mi%20WhatsApp%20es%20${whatsapp}.%20%20Mi%20correo%20es%20${email}.%20Mensaje:%20${mensaje}`;
     
     window.open(whatsappURL, "_blank");
 });
